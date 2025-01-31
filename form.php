6ff8720448749568c193584e357c191598f6a5bd
secretlabrat <secretlaboratory333@gmail.com>
2025-01-30 22:20:49 -0800

    change result.php to process.php

--- form.php
+++ form.php
@@ -1,4 +1,4 @@
-<form action="result.php" method="POST">
+<form action="process.php" method="POST">
 a: <input type="number" name="a"><br>
 b: <input type="number" name="b"><br>
 c: <input type="number" name="c"><br>
